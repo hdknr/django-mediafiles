27746886bd2e6d21d472b58c75fc937c5cf3fb4a
hdknr <gmail@hdknr.com>
2013-07-31 11:51:31 +0900

    image and thumbnail url maker

--- src/mediafiles/templatetags/mediafiles.py
+++ src/mediafiles/templatetags/mediafiles.py
@@ -19,3 +19,27 @@ def make_delete_url(context,urlname,**kwargs ):
     p = context.get('mediafile_deleter_hint',{} )
     p.update(kwargs)
     return reverse(urlname, kwargs=p)
+#
+@register.simple_tag(takes_context=True)
+def mediafile_delete_url(context,mediafile,**kwargs ):
+    urlname = context.get('mediafile_delete_url','gallery_admin_media_delete')
+    p = context.get('mediafile_url_hint',{} )
+    p.update(kwargs)
+    return reverse(urlname, kwargs=p)
+
+@register.simple_tag(takes_context=True)
+def mediafile_image_url(context,mediafile,**kwargs ):
+    urlname = context.get('mediafile_image_url','mediafiles_preview')
+    p = context.get('mediafile_url_hint',{} )
+    p.update(kwargs)
+    return reverse(urlname, kwargs=p)
+
+@register.simple_tag(takes_context=True)
+def mediafile_thumbnail_url(context,mediafile,**kwargs ):
+    if mediafile.is_image() == False:
+        return mediafile.get_thumbnail_url()
+
+    urlname = context.get('mediafile_thumbnail_url','mediafiles_preview')
+    p = context.get('mediafile_url_hint',{} )
+    p.update(kwargs)
+    return reverse(urlname, kwargs=p)
